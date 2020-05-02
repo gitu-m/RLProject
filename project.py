4c35ad9e386f4bfd34926cbdc77b02e98c647718
gitu-m <cs16btech11014@iith.ac.in>
2020-05-02 18:48:45 +0530

    Add pachi. TODO: mak it actually play *facepal*

--- project.py
+++ project.py
@@ -1,7 +1,10 @@
 import numpy as np
 import torch
 import gym
+import pachi_py
+import inspect
 from os.path import join
+
 from policy_network import Policy_network
 from policy_network import Policy_grad
 
@@ -11,8 +14,18 @@ PATH = "./model.pt"
 
 go_env = gym.make('gym_go:go-v0', size=5, reward_method='heuristic')
 
+# go_9 = gym.make('Go19x19-v0')
+
+b = pachi_py.CreateBoard(5).play(24, pachi_py.BLACK)
+
+engine = pachi_py.PyPachiEngine(b, b'uct', b'')
+pachi_move = (engine.genmove(pachi_py.BLACK, b'1'))
+
+b.play(pachi_move, pachi_py.WHITE)
+print(pachi_move)
+
 # Train 5x5 model
-Policy_grad(100, 10, 0.001, 0.99, go_env, False, True)
+# Policy_grad(100, 10, 0.001, 0.99, go_env, False, True)
 
 # Load trained model
 policy_5x5 = torch.load(PATH)
